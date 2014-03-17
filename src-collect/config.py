da6128c4f3af52d68d5800c574d8d20e92920a4c
Alexandra Olteanu <alexandra.olteanu@epfl.ch>
2014-03-17 11:16:39 +0100

    allow adding more terms to be tracked

--- src-collect/config.py
+++ src-collect/config.py
@@ -7,5 +7,8 @@ ACCESS_SECRET= ''
 #path to the file containing the lexicon
 input_filename = '../data/crisislex-recommended-v1.txt'
 
+#path to the file containing the additional terms you want to track
+additional_terms_file = ''
+
 #the output is in json format; if an empty string is given, the tweets are saved in "your_json_file.json" in the current directory
 output_filename = ''
