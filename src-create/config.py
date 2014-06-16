0a44f91e217a5db191dbfd27185f3e42dc3d39e7
Alexandra Olteanu <alexandra.olteanu@epfl.ch>
2014-06-17 00:01:27 +0200

    lexicon building: read & config

---
+++ src-create/config.py
@@ -0,0 +1,10 @@
+# replace with the label of class for which you are interested in building the lexicon;
+# this should be the same as the label in your input files
+positive_class_label = "on-topic"
+
+# replace the label for the examples that do not belong to the topic of interest
+# this should be the same as the label in your input files
+negative_class_label ="off-topic"
+
+# lexicon size
+lexicon_size = 400
---
+++ src-create/read.py
@@ -0,0 +1,91 @@
+# CrisisLex
+# Author: Alexandra Olteanu
+# Check LICENSE for details about copyright.
+
+import csv
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+def get_stemmed_terms_list(doc, stem_words_map = None, stem_bigrams_map = None):
+    ps = PorterStemmer()
+    local_map = dict()
+    word_list = []
+
+    clean_doc = [(w.strip()).lower() for w in doc.split() if len(w) in range(3,16)]
+    filtered_words = [w.strip('.,;?!:)(#') for w in clean_doc if not w.strip('.,;?!:)(#') in stopwords.words('english')]
+
+    for w in filtered_words:
+        if w.isalpha():
+            w_temp = ps.stem_word(w)
+            if stem_words_map is not None:
+                if w_temp not in stem_words_map:
+                    stem_words_map[w_temp] = dict()
+                stem_words_map[w_temp][w] = stem_words_map[w_temp].get(w, 0)+1
+                local_map[w_temp] = w
+            word_list.append(w_temp)
+
+    bigrams = nltk.bigrams(word_list)
+    for b in bigrams:
+        bigram_org = (local_map[b[0]],local_map[b[1]])
+        if stem_bigrams_map is not None:
+                if b not in stem_bigrams_map:
+                    stem_bigrams_map[b] = dict()
+                stem_bigrams_map[b][bigram_org] = stem_bigrams_map[b].get(bigram_org, 0)+1
+    return word_list, bigrams
+
+# keeps track of the exact form of the stemmed bigrams, not only the one of the words
+def get_tweet_terms(tweet, stem_map = None, bigrams_map = None):
+    words, bigrams = get_stemmed_terms_list(tweet, stem_map, bigrams_map)
+    filtered_words = [w for w in words if not w in stopwords.words('english')]
+    bigrams = nltk.bigrams(filtered_words)
+    words_set = set(filtered_words)
+    terms_dict = {}
+    for w in words_set:
+        terms_dict['%s'%w] = 'y'
+
+    for b in bigrams:
+        terms_dict['%s-%s'%(b[0],b[1])] = 'y'
+    return terms_dict
+
+def get_terms(ifile, min_occurence = 0.5, stem_map = None, bigrams_map = None):
+    tweets_cls = []
+    tweets_type = []
+    tweets_terms = []
+    tweets_id = []
+    tweets_no = 0
+    ws = set()
+    wd_occ = dict()
+    fd = nltk.FreqDist()
+    r = csv.reader(ifile)
+    print "Reading..."
+    for tokens in r:
+        id = tokens[0].strip()
+        tweet = tokens[1].strip()
+        cls = tokens[2].strip()
+        terms = get_tweet_terms(tweet, 2, stem_map, bigrams_map)
+
+        tweets_cls.append(cls)
+        tweets_type.append(type)
+        tweets_id.append((id,tweet))
+        tweets_terms.append(terms)
+        [fd.inc(x) for x in terms]
+        ws.update(set(terms.keys()))
+
+    print "Cleaning..."
+    for t in tweets_terms:
+        s = set()
+        term = t.keys()
+        l = len(term)
+        for i,f in enumerate(term):
+            if fd[f] <= min_occurence*tweets_no:
+                s.add(f)
+            if l > 1:
+                if i == l-1:
+                    break
+                for j in range(i+1, l):
+                    wd_occ[(f, term[j])] = wd_occ.get((f, term[j]), 0)+1
+        for f in s:
+            del t[f]
+            ws.discard(f)
+    return tweets_cls, tweets_terms, wd_occ, ws, fd
